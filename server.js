dc59fe38ac0975729da229d3106be0a62b41a2c6
Jariane Santos <jrnjrn1234@gmail.com>
2026-08-27 16:21:49 -0300

    Replace bcrypt with bcryptjs for Render compatibility

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const nodemailer = require('nodemailer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
